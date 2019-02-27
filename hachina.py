64843a0f7fb8be6f608b0339538dd116257d950d
yinzn <yinzn@guahao.com>
2019-02-27 17:58:05 +0800

    for learn

--- hachina.py
+++ hachina.py
@@ -1,16 +1,21 @@
 """
-文件名：hachina.py.
+文件名 hachina.py.
 
-演示程序，三行代码创建一个新设备.
+演示程序，增加设备的属性值.
 """
+
+# HomeAssistant的惯例，会在组件程序中定义域，域与组件程序名相同
+DOMAIN = "hachina"
 
 
 def setup(hass, config):
-    """HomeAssistant在配置文件中发现hachina域的配置后，会自动调用hachina.py文件中的setup函数."""
-    # 设置实体hachina.Hello_World的状态。
-    # 注意1：实体并不需要被创建，只要设置了实体的状态，实体就自然存在了
-    # 注意2：实体的状态可以是任何字符串
-    hass.states.set("hachina.hello_world", "太棒了！")
+    """配置文件加载后，被调用的程序."""
+    # 准备一些属性值，在给实体设置状态的同时，设置实体的这些属性
+    attr = {"icon": "mdi:yin-yang",
+            "friendly_name": "迎接新世界",
+            "slogon": "积木构建智慧空间！"}
 
-    # 返回True代表初始化成功
+    # 使用了在程序开头预定义的域
+    # 设置状态的同时，设置实体的属性
+    hass.states.set(DOMAIN+".hello_world", "太棒了！", attributes=attr)
     return True
